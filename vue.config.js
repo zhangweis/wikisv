11fcb81f73a691643af5142b295d56317777cedb
zhangwei <cdzhangw@gmail.com>
2019-09-10 09:44:45 +0800

    add inliner to vue build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+var Inliner = require('inliner');
+const exec = require('child_process').exec;
+const fs = require('fs');
 
-console.log();
 var externals = {
 		vue:'Vue',
 		'vue-moment':'vueMoment',
@@ -53,7 +55,23 @@ optimization: {
 chainWebpack: config => {
 		    config.plugins.delete('preload');
 if (process.env.NODE_ENV === "production")
-        config.plugin("webpack-report")
+config.plugin("inline").use(
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+
+var inliner = new Inliner('dist/index.html',{skipAbsoluteUrls:true,
+compressJS:false,
+compressCSS:false}, (error,html)=>{
+fs.writeFile('/home/zhangwei/githubbts/wikisv/dist/wikisv.html',html, ()=>{
+console.log('inline done.');
+});
+});
+        });
+      }
+    });
+
+		config.plugin("webpack-report")
             .use(BundleAnalyzerPlugin, [{
                 // ...webpack-bundle-analyzer options here
             }]);
